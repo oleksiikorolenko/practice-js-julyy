c705e039ea6bcc98a4b595dfe91cf31a784cfc05
Korolenko <korolenkoalexey84@gmail.com>
2025-07-24 15:28:26 +0300

    thirsday commit

--- js/lesson-js-02.js
+++ js/lesson-js-02.js
@@ -251,25 +251,25 @@
 // Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою
 // назвою фрукта, це також треба урахувати.
 
-const fruits = [
-    { name: "Яблуко", price: 45, quantity: 7 },
-    { name: "Апельсин", price: 60, quantity: 4 },
-    { name: "Банан", price: 125, quantity: 8 },
-    { name: "Груша", price: 350, quantity: 2 },
-    { name: "Виноград", price: 440, quantity: 3 },
-    { name: "Банан", price: 125, quantity: 3 },
-  ];
+// const fruits = [
+//     { name: "Яблуко", price: 45, quantity: 7 },
+//     { name: "Апельсин", price: 60, quantity: 4 },
+//     { name: "Банан", price: 125, quantity: 8 },
+//     { name: "Груша", price: 350, quantity: 2 },
+//     { name: "Виноград", price: 440, quantity: 3 },
+//     { name: "Банан", price: 125, quantity: 3 },
+//   ];
 
-function calcTotalPrice(fruits, fruitName) {
-    let total = 0;
+// function calcTotalPrice(fruits, fruitName) {
+//     let total = 0;
     
-    for (const fruit of fruits) {
-        if (fruit.name === fruitName) {
-            total += fruit.price * fruit.quantity;
-        }
-    }
-    return total;
-}
+//     for (const fruit of fruits) {
+//         if (fruit.name === fruitName) {
+//             total += fruit.price * fruit.quantity;
+//         }
+//     }
+//     return total;
+// }
 
 
-console.log(calcTotalPrice(fruits, "Виноград"));
+// console.log(calcTotalPrice(fruits, "Виноград"));
